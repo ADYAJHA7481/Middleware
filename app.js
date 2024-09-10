f5a7e381ec73abf7fd03e5bc979a3042a36e3bf1
Aditya Kashyap <145245185+ADYAJHA7481@users.noreply.github.com>
2024-09-10 19:12:59 +0530

    Next(),Error MiddleWare

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require("express")
 const app = express();
 
-app.use((req,res,next) =>{
-    req.time = new Date(Date.now()).toString();
-    console.log(req.method,req.hostname,req.time);
-    next()
-})
+//logger - morgan
+// app.use((req,res,next) =>{
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method,req.hostname,req.time);
+//     next()
+// })
+
+const checkToken = (req,res,next)=>{
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }
+    throw new Error("ACCESS DENIED!");
+};
+
+// app.get("/random", (req,res,next)=>{
+//     console.log("Hii, I am random page");
+//     next();
+// });
+
+// app.get("/wrong",(req,res)=>{
+//     abcd = abcd;
+// })
+
+app.get("/api", checkToken, (req,res)=>{
+    res.send("Data");
+});
 
 app.get("/", (req,res)=>{
     console.log("Hii, I am root");
 });
 
-app.get("/random", (req,res)=>{
-    console.log("Hii, I am random page");
+app.use((err,req,res,next)=>{
+    console.log(err);
+});
+
+app.use((req,res)=>{
+    res.send("Page not found!!");
 });
 
 app.listen(8080,()=>{
